2f9fb6d096819b1bc3490960478a4fccbb460172
Ilyas Hasanoff <45775763+IlyasKhanHasanoff@users.noreply.github.com>
2023-04-27 16:18:50 -0400

    Accidently commited a broken version of code

--- program.java
+++ program.java
@@ -1,45 +1,48 @@
 import java.util.*;
 public class program {
+	static Scanner input = new Scanner(System.in);
+  
     public static void main(String[] args) {
         sum();
     }
-
-    public static void sum() {
-        Scanner input = new Scanner(System.in);
-        double[] array = loop(input);
-        double add = 0;
-        for (int i = 0; i < array.length; i++) {
-            add += array[i];
-        }
-        System.out.println("The Store Will Make $" + add);
-        String title = busName(input);
-        System.out.println("Store Name: " + title);
-        input.close();
+    public static double test() {
+        System.out.print("Enter wholesale cost $");
+        double wholesaleCost = input.nextDouble();
+        System.out.print("Enter markup precentage: ");
+        double markUp = (input.nextDouble()/100) + 1;
+        double result = calculateRetail(wholesaleCost, markUp);
+        System.out.println("Retail Cost is $"+result);
+        return result;
     }
-
-    public static void loop(Scanner input) {
-        System.out.print("Enter number of Items: ");
-        int num = input.nextInt();
-        double[] doubles = new double[num];
-        for (int i = 0; i < doubles.length; i++) {
-            System.out.print("Enter wholesale cost $");
-            double wholesaleCost = input.nextDouble();
-            System.out.print("Enter markup percentage: ");
-            double markUp = (input.nextDouble() / 100) + 1;
-            double result = calculateRetail(wholesaleCost, markUp);
-            System.out.println("Retail Cost is $" + result);
-            doubles[i] = result;
-        }
-        System.out.println(Arrays.toString(doubles));
-        System.out.println("Number of Items: " + num);
-    }
-
-    public static double calculateRetail(double wholesaleCost, double markUp) {
+    public static double calculateRetail(double markUp, double wholesaleCost) {
         double retail = markUp * wholesaleCost;
         return retail;
     }
+    public static double[] loop() {
+        System.out.print("Enter number of Items: ");
+        int num = input.nextInt();
+        double[] doubles = new double[num];
+        for(int i = 0; i < doubles.length; i++){
+            double run = test();
+            doubles[i] = run;
+        }
+        System.out.println(Arrays.toString(doubles));
+        System.out.println("Number of Items: "+num);
+        return doubles;
+    }
+    public static double sum(){
+        double[] array = loop();
+        double add = 0;
+        for(int i=0; i < array.length; i++){
+            add = add += array[i];
+         }
+         System.out.println("The Store Will Make $"+ add);
+         String title = busName();
+         System.out.println("Store Name:"+ title);
+         return add;
 
-    public static String busName(Scanner input) {
+    }
+    public static String busName(){
         System.out.println("Enter Name of Business: ");
         input.nextLine();
         String name = input.nextLine();
